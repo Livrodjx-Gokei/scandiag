e5c86f377cbb31571461aa469db2e37bee244ec3
Livrodjx-Gokei <lucas.alexandre@gokeitecnologia.com.br>
2023-07-07 17:29:58 -0300

    Adding

--- Form1.cs
+++ Form1.cs
@@ -16,12 +16,26 @@ namespace scandiag
         public Form1()
         {
             InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
             rk.SetValue("scandiag", Application.ExecutablePath);
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            const int WM_SYSKEYDOWN = 0x0104;
+            const int VK_F4 = 0x73; 
+
+            if ((msg.Msg == WM_SYSKEYDOWN) && (keyData == (Keys.Alt | Keys.F4)))
+            {
+                return true; // Impede o fechamento do aplicativo
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
         }
     }
 }
